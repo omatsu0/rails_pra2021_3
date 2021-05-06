1c3d5304220f568e39ec7609bb2cf4eb04212ee4
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-05-06 22:54:01 +0900

    add enum to book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_142901) do
+ActiveRecord::Schema.define(version: 2021_05_06_135258) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
