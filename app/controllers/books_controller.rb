c64989e12712a0df15065a35427ab338a6e2fb16
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-05-12 22:37:16 +0900

    add set_book action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
   def show
-    @book = Book.find(params[:id])
     respond_to do |format|
       format.html
       format.json
@@ -8,11 +7,17 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book=Book.find(params[:id])
     @book.destroy
     respond_to do |format|
       format.html { redirect_to "/" }
       format.json { head :no_content }
     end
   end
+
+  private
+  
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
 end
